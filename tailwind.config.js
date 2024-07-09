598b65df6e732a5ec04c9918a0144ea55bd0ea20
niketski <lim.nicolebenedict3@gmail.com>
2024-07-09 19:48:36 +0800

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
