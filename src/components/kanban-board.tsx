fcbef9eba5676b65ab693f4bb340e751d6bd600a
niketski <lim.nicolebenedict3@gmail.com>
2024-07-11 20:15:21 +0800

    feat: created static data for task and columns

--- src/components/kanban-board.tsx
+++ src/components/kanban-board.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import KanbanColumn from "./kanban-column"
-import { Column } from "../types";
+import { Column, Task } from "../types";
 
 const INITIAL_COLUMNS: Column[] = [
   {
@@ -24,8 +24,43 @@ const INITIAL_COLUMNS: Column[] = [
     title: 'Column 5'
   }
 ];
+
+const INITIAL_TASKS: Task[] = [
+  {
+    id: 1,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    priority: 'high',
+    columnId: 1
+  },
+  {
+    id: 2,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    priority: 'low',
+    columnId: 1
+  },
+  {
+    id: 3,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    priority: 'low',
+    columnId: 2
+  },
+  {
+    id: 4,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    priority: 'high',
+    columnId: 4
+  },
+  {
+    id: 5,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    priority: 'medium',
+    columnId: 3
+  }
+];
+
 function KanbanBoard() {
   const [columns] = useState<Column[]>(INITIAL_COLUMNS);
+  const [ tasks ] = useState<Task[]>(INITIAL_TASKS);
 
   return (
     <div className="pt-4 pb-5">
@@ -35,11 +70,14 @@ function KanbanBoard() {
           {
             columns && 
               columns.map(column => {
+                const filteredTasks = tasks.filter(task => task.id === column.id); // filter task based on the corresponding column
+
                 return (
                   <KanbanColumn 
                     key={column.id}
                     id={column.id}
-                    title={column.title}/>
+                    title={column.title}
+                    tasks={filteredTasks}/>
                 )
               })
           }
--- src/components/kanban-column.tsx
+++ src/components/kanban-column.tsx
@@ -1,9 +1,10 @@
-import { Id } from "../types"
-import { Trash2, Plus } from "lucide-react"
+import { Id, Task } from "../types"
+import { Trash2, Plus, GripVertical } from "lucide-react"
 
 interface KanbanColumnProps {
     title: string,
     id: Id
+    tasks: Task[]
 }
 
 function KanbanColumn({ title } : KanbanColumnProps) {
@@ -13,6 +14,9 @@ function KanbanColumn({ title } : KanbanColumnProps) {
             {/* column heading */}
             <div className="bg-primary flex justify-between items-center py-3 px-4 rounded-tl-lg rounded-tr-lg">
                 <div className="flex items-center">
+                    <button className="text-white mr-2 outline-none border-none cursor-grab">
+                        <GripVertical/>
+                    </button>
                     <h3 className="text-white font-bold">{title}</h3>
                     <span className="bg-white text-primary py-1 px-2 text-[14px] leading-none rounded-lg inline-block ml-3">5</span>
                 </div>
@@ -30,17 +34,70 @@ function KanbanColumn({ title } : KanbanColumnProps) {
             {/* column body */}
             <div className=" flex-1 overflow-y-auto p-4">
                 <div className="flex flex-col">
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
-                    <div>Task</div>
+
+                    <div className="py-2">
+                        <div className="rounded-lg shadow bg-white px-4 break-words pt-5 pb-4">
+                            <div className="flex justify-between items-center mb-5">
+                                <div className="flex items-center">
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none mr-2" title="Delete task">
+                                        <GripVertical/>
+                                    </button>
+                                    <p className="text-[12px]">Priority: <span className="font-bold inline-block ml-2 px-2 py-1 bg-[#55efc4] leading-none rounded-lg text-white">Low</span></p>
+                                </div>
+                                <div>
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none" title="Delete task">
+                                        <Trash2/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="py-2">
+                        <div className="rounded-lg shadow bg-white px-4 break-words pt-5 pb-4">
+                            <div className="flex justify-between items-center mb-5">
+                                <div className="flex items-center">
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none mr-2" title="Delete task">
+                                        <GripVertical/>
+                                    </button>
+                                    <p className="text-[12px]">Priority: <span className="font-bold inline-block ml-2 px-2 py-1 bg-[#fdcb6e] leading-none rounded-lg text-[12px] text-white">Medium</span></p>
+                                </div>
+                                <div>
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none" title="Delete task">
+                                        <Trash2/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="py-2">
+                        <div className="rounded-lg shadow bg-white px-4 break-words pt-5 pb-4">
+                            <div className="flex justify-between items-center mb-5">
+                                <div className="flex items-center">
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none mr-2" title="Delete task">
+                                        <GripVertical/>
+                                    </button>
+                                    <p className="text-[12px]">Priority: <span className="font-bold inline-block ml-2 px-2 py-1 bg-[#ff7675] leading-none rounded-lg text-[12px] text-white">High</span></p>
+                                </div>
+                                <div>
+                                    <button className="text-gray-400 hover:opacity-70 transition-opacity outline-none" title="Delete task">
+                                        <Trash2/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 
             </div>
